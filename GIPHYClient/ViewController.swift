98ae3430a616353aa00a7663724c9135d816fbd2
Bryan A <bolivarbryan@gmail.com>
2019-02-21 23:21:38 -0500

    First Commit, project created

---
+++ GIPHYClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GIPHYClient
+//
+//  Created by Bryan A Bolivar M on 2/21/19.
+//  Copyright © 2019 BolivarBryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
